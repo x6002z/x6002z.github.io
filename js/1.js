74576818f85c5bf127ced0c1d6ac76fc98530004
x6002z <419499770@qq.com>
2016-08-06 12:14:11 +0800

    第一次未完成ban

---
+++ js/1.js
@@ -0,0 +1,7 @@
+// var oBan = document.getElementsByTagName('ul')[0]
+// var aBan = oBan.children
+// // var oL1 = oBan.getElementsByTagName('li')[0]
+// function autoChange(obj) {
+//     var child = obj.children
+// }
+
